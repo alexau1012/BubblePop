5ed926c436641dfcb75e2cf9fb40e56b9c31d433
Alex Au <alexaumh@gmail.com>
2021-04-18 23:46:13 +1000

    Create all view controller classes

--- BubblePop/ViewController.swift
+++ BubblePop/ViewController.swift
@@ -7,13 +7,72 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+}
+
+class SettingsViewController: UIViewController {
+
+    @IBOutlet weak var nameText: UITextField!
+    @IBOutlet weak var gameTimeSlider: UISlider!
+    @IBOutlet weak var maxNumBubblesSlider: UISlider!
+
+    @IBOutlet weak var gameTimeLabel: UILabel!
+    @IBOutlet weak var maxNumBubblesLabel: UILabel!
+    
+    @IBOutlet weak var start: UIButton!
+    
+    var name: String?;
+    var gameTime: Float?;
+    var maxNumBubbles: Float?;
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        gameTimeSlider.value = 60;
+        gameTimeLabel.text = String(Int(gameTimeSlider.value));
+        maxNumBubblesSlider.value = 15;
+        maxNumBubblesLabel.text = String(Int(maxNumBubblesSlider.value));
+    }
+
+    @IBAction func nameTextOnChange(_ sender: Any) {
+        name = nameText.text;
+    }
+    
+    @IBAction func gameTimeSliderOnChange(_ sender: Any) {
+        gameTime = gameTimeSlider.value;
+        gameTimeLabel.text = String(Int(gameTimeSlider.value));
+    }
+    
+    @IBAction func maxNumBubblesSliderOnChange(_ sender: Any) {
+        maxNumBubbles = maxNumBubblesSlider.value;
+        maxNumBubblesLabel.text = String(Int(maxNumBubblesSlider.value));
+    }
+    
+    @IBAction func startOnClick(_ sender: Any) {
+        performSegue(withIdentifier: "startSegue", sender: nil);
+    }
+}
+
+class PlayViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
 
 }
 
+class HighScoreViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
